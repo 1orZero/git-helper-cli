5332ac39d0c407316c8f62f976205673b60373d6
1orzero <laokameng.6@gmail.com>
2025-02-25 16:47:40 +0800

    feat(cli): integrate commit message generation into CLI

--- internal/cli/commands.go
+++ internal/cli/commands.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/1orzero/git-helper-cli/internal/branch"
+	"github.com/1orzero/git-helper-cli/internal/commit"
 	"github.com/1orzero/git-helper-cli/internal/state"
 	"github.com/1orzero/git-helper-cli/internal/utils"
 	ucli "github.com/urfave/cli/v2"
@@ -13,9 +14,16 @@ func Commands(appState *state.AppState) []*ucli.Command {
 	return []*ucli.Command{
 		{
 			Name:      "generate-branch-name",
+			Aliases:   []string{"gb"},
 			Usage:     "Generate a branch name based on the issue type and title",
 			Action:    generateBranchName(appState),
 			ArgsUsage: "<description>",
+		},
+		{
+			Name:    "generate-commit",
+			Aliases: []string{"gc"},
+			Usage:   "Generate commit messages for staged changes",
+			Action:  generateCommit(appState),
 		},
 	}
 }
@@ -34,6 +42,21 @@ func generateBranchName(appState *state.AppState) func(c *ucli.Context) error {
 		utils.CopyToClipboard(selectedBranch)
 
 		Output(selectedBranch)
+		return nil
+	}
+}
+
+func generateCommit(appState *state.AppState) func(c *ucli.Context) error {
+	return func(c *ucli.Context) error {
+		commitMessages, err := commit.GenerateCommitMessages(*appState.LLM)
+		if err != nil {
+			return ucli.Exit("Error generating commit messages: "+err.Error(), 1)
+		}
+
+		selectedCommit := commit.SelectCommitMessage(commitMessages)
+		utils.CopyToClipboard(selectedCommit)
+
+		Output(selectedCommit)
 		return nil
 	}
 }
